8417796a83c21865090b4ae837d4d527c38a702e
db <dblee@ipartners.co.kr>
2020-07-08 16:58:47 +0900

    0708 upload용

--- portfolio_project/제이슨 여행사 프로젝트_swipe_Bom/js/section4Slide.js
+++ portfolio_project/제이슨 여행사 프로젝트_swipe_Bom/js/section4Slide.js
@@ -1,10 +1,10 @@
 (function($,window,document,undefined){
-    
+
     // rolling slide
     var cnt = 0,
         setId,
         slideCont = $('.s4Slide-container'),
-        
+
         winW = $(window).innerWidth(),
         imgN = 3; //기본 3개
         slideConWidth = $('.s4Slide-container').innerWidth(); //화면에 나오는 컨테이너 박스 1570
@@ -17,12 +17,12 @@
             else{ // 600 이하
                 imgN = 1;
             }
-        
+
         slideWidth =  slideConWidth / imgN; //523.33333 슬라이드 너비
         slideWrap = $('.s4Slide-wrap'), //rolling을위해
         slide = $('.s4Slide'), //rolling을위해
-        slideImg = $('.s4Slide .image'), 
-        slideImgWidth = $('.s4Slide .caption').innerWidth()*0.59450414, //높이를 구하기 위해서 
+        slideImg = $('.s4Slide .image'),
+        slideImgWidth = $('.s4Slide .caption').innerWidth()*0.59450414, //높이를 구하기 위해서
         pageBtn = $('.pageBtn');
 
         //6.반응형 적용
@@ -34,7 +34,7 @@
         function resizeFn(){
             //계산값
             winW = $(window).innerWidth();
-            slideConWidth = $('.s4Slide-container').innerWidth(); 
+            slideConWidth = $('.s4Slide-container').innerWidth();
             if( winW > 1024){ //1024 초과
                 imgN = 3;
             }
@@ -44,12 +44,12 @@
             else{ // 600 이하
                 imgN = 1;
             }
-        
 
-            slideWidth =  slideConWidth / 3; 
+
+            slideWidth =  slideConWidth / 3;
             slideImgWidth = $('.s4Slid .caption').innerWidth()*0.59450414, //지금 현재의 너비
 
-            
+
             slide.css({width: slideWidth});
             slideWrap.css({width: (slideWidth*12), marginLeft: (-slideWidth*3)});
             slideImg.css({width: slideWidth, height: slideImgWidth});
@@ -57,6 +57,10 @@
             // 반응형이 슬라이드메인함수에 적용이 되어서 left값이 맞게 된다.
             slideWrap.stop().animate({left: -523.3333333*cnt},0);
         }
+
+
+
+
 
         resizeFn();
         setTimeout(resizeFn,100);
@@ -70,7 +74,7 @@
         function mainSlideFn(){
             slideWrap.stop().animate({left: -523.3333333*cnt},800,function(){ //6개의 슬라이드가 있으니 callback함수
                 if(cnt > 5){cnt = 0}
-                if(cnt < 0){cnt = 5} 
+                if(cnt < 0){cnt = 5}
                 slideWrap.stop().animate({left: -523.3333333*cnt},0);
             }); //css에서 position:relative꼭 확인! transform은 익스9이하는 적용이 안됨. 사파리도 안됨
 
@@ -81,7 +85,7 @@
             cnt > 5 ? z = 0 : z = cnt;
             pageBtn.removeClass('addPageBtnS4');
             pageBtn.eq(cnt).addClass('addPageBtnS4');
-            
+
         }
 
         // 2-1. 다음 슬라이드 카운트 함수
@@ -109,15 +113,15 @@
                     clearInterval(setId);
                     prevSlideCountFn();
                 }
-            }    
+            }
         });
-    
-    // 4. 인디게이터 클릭 이벤트 
+
+    // 4. 인디게이터 클릭 이벤트
         pageBtn.each(function(index){
             $(this).on({
                 click : function(){
                     clearInterval(setId);
-                    cnt = index; 
+                    cnt = index;
                     mainSlideFn();
                 }
             });
